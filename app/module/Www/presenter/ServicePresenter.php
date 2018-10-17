514d24b1530ab1e27311bced52260e4570889902
Vaclav Pelisek <peldax@gmail.com>
2018-10-17 13:23:16 +0200

    změny presenterů a komponent

--- app/module/Www/presenter/ServicePresenter.php
+++ app/module/Www/presenter/ServicePresenter.php
@@ -4,8 +4,58 @@ declare(strict_types = 1);
 
 namespace App\AdminModule\Presenter;
 
+use App\WwwModule\Component\IServiceListFactory;
+use App\WwwModule\Component\ISubServiceListFactory;
+use App\WwwModule\Component\ServiceList;
+use App\WwwModule\Component\SubServiceList;
 use Nepttune\Presenter\BaseAuthPresenter;
 
 final class ServicePresenter extends BaseAuthPresenter
 {
+    /** @var IServiceListFactory */
+    protected $IServiceListFactory;
+
+    /** @var ISubServiceListFactory */
+    protected $ISubServiceListFactory;
+
+    public function __construct(
+        IserviceListFactory $IServiceListFactory,
+        ISubServiceListFactory $ISubServiceListFactory)
+    {
+        parent::__construct();
+
+        $this->IServiceListFactory = $IServiceListFactory;
+        $this->ISubServiceListFactory = $ISubServiceListFactory;
+    }
+
+    protected function createComponentServiceList() : ServiceList
+    {
+        return $this->IServiceListFactory->create();
+    }
+
+    protected function createComponentSubServiceList() : SubServiceList
+    {
+        $control = $this->ISubServiceListFactory->create();
+        $control->setId($this->getId());
+        return $control;
+    }
+
+    /**
+     * @restricted
+     * @root
+     */
+    public function actionDefault() : void
+    {
+
+    }
+
+    /**
+     * @restricted
+     * @root
+     */
+    public function actionSub(int $id) : void
+    {
+
+    }
 }
+
