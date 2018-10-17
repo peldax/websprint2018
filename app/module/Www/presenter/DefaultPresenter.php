a195ff9cb2831d9ed307072eeac05cffc240a39c
Vaclav Pelisek <peldax@gmail.com>
2018-10-17 15:44:58 +0200

    default presenter

--- app/module/Www/presenter/DefaultPresenter.php
+++ app/module/Www/presenter/DefaultPresenter.php
@@ -4,10 +4,28 @@ declare(strict_types = 1);
 
 namespace App\AdminModule\Presenter;
 
+use App\AdminModule\Component\EventList;
+use App\AdminModule\Component\EventForm;
+use App\AdminModule\Component\IEventListFactory;
+use App\AdminModule\Component\IEventFormFactory;
 use Nepttune\Presenter\BaseAuthPresenter;
 
 final class DefaultPresenter extends BaseAuthPresenter
 {
+    /** @var IEventListFactory */
+    protected $iEventListFactory;
+
+    /** @var IEventFormFactory */
+    protected $iEventFormFactory;
+
+    public function __construct(IEventListFactory $IEventListFactory, IEventFormFactory $IEventFormFactory)
+    {
+        parent::__construct();
+
+        $this->iEventListFactory = $IEventListFactory;
+        $this->iEventFormFactory = $IEventFormFactory;
+    }
+
     public function actionDefault() : void
     {
         if ($this->user->isInRole('room'))
@@ -18,5 +36,15 @@ final class DefaultPresenter extends BaseAuthPresenter
         {
             $this->template->setFile(__DIR__ . '/../templates/Default/admin.latte');
         }
+    }
+
+    protected function createComponentEventList() : EventList
+    {
+        return $this->iEventListFactory->create();
+    }
+
+    protected function createComponentEventForm() : EventForm
+    {
+        return $this->iEventFormFactory->create();
     }
 }
